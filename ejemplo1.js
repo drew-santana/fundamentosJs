846fec638af666e6c26f90df6776fe8c6e69cc43
Andres Santana <drew.lizcano@gmail.com>
2019-07-31 21:31:48 -0500

    encasulando reguest en función, tuto adelantado

--- ejemplo1.js
+++ ejemplo1.js
@@ -3,8 +3,8 @@ const REFERENCE_URL = 'people/:id'
 var id = prompt('Qué id quieres ?')
 const PeopleURL = `${API_URL}${REFERENCE_URL.replace(':id',id)}` 
 const opts = {crossDomain: true}
-const onPeopleRespnse = function(data){
-  console.log(`${data.name}`)
+const onPeopleRespnse = function(data){ 
+  console.log(`Hola, soy ${data.name} y mido ${data.height} centimetros`)
 }
 $.get(PeopleURL,opts,onPeopleRespnse)
 
