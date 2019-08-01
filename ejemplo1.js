502cae5613a4a6241c526fd814665b1513e9a94a
Andres Santana <drew.lizcano@gmail.com>
2019-08-01 17:17:30 -0500

    agregando .fail

--- ejemplo1.js
+++ ejemplo1.js
@@ -1,10 +1,20 @@
 const API_URL = 'https://swapi.co/api/'
 const REFERENCE_URL = 'people/:id'
-var id = prompt('Qué id quieres ?')
-const PeopleURL = `${API_URL}${REFERENCE_URL.replace(':id',id)}` 
+ 
 const opts = {crossDomain: true}
-const onPeopleRespnse = function(data){ 
+
+const onPeopleResponse = function(data){ 
   console.log(`Hola, soy ${data.name} y mido ${data.height} centimetros`)
 }
-$.get(PeopleURL,opts,onPeopleRespnse)
+const onError = function(id){
+  console.log(`No se pudo obtener el persoaje#${id}`)
+}
+
+
+  function ObtenerPersonaje(id){
+  var id = prompt('Qué id quieres ?')
+  const PeopleURL = `${API_URL}${REFERENCE_URL.replace(':id',id)}`
+  $.get(PeopleURL,opts,onPeopleResponse)
+  .fail(onError)
+}
 
