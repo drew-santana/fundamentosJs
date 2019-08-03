22b1a9c01e96c628c46bf5ad815b13fb4607f856
Camilo <drew.lizcano@gmail.com>
2019-08-02 21:27:14 -0500

    promesas multiples

--- ejemplo1.js
+++ ejemplo1.js
@@ -1,9 +1,43 @@
 const API_URL = 'https://swapi.co/api/'
 const REFERENCE_URL = 'planets/3'
-const PeopleURL = `${API_URL}${REFERENCE_URL.replace('3',1)}` 
+
 const opts = {crossDomain: true}
-const onPeopleRespnse = function(data){
+
+
+function ObtenerPersonaje(id){
+  return new Promise((resolve,reject)=>{
+    const URL = `${API_URL}${REFERENCE_URL.replace('3',id)}` 
+    $.get(URL,opts,function(data){
+      resolve(data)
+    })
+    .fail(()=> reject(id))
+  })
+}
+const onPeopleResponse = function(data){
   console.log(`${data.name}`)
 }
-$.get(PeopleURL,opts,onPeopleRespnse)
+const onError = function(id){
+  console.log(`No se pudo obtener el personaje ${id} `)
+}
+//cómo preguntar el numero de busquedas ?  
+var ids = [1,2,3,4,5,6,7]
+var promesas = ids.map((id)=> ObtenerPersonaje(id))
 
+function obtenerPersonajes(personajes){
+ for(var i = 0; i<personajes.length; i++ ){
+   console.log(personajes[i].name)
+ }
+  
+}
+Promise.
+all(promesas)
+.then(personajes => obtenerPersonajes(personajes))
+.catch(onError)
+
+
+// ObtenerPersonaje(1)
+// .then((data)=>{
+//   onPeopleResponse(data)
+//   return ObtenerPersonaje(2)
+// }).then(onPeopleResponse)
+// .catch(onError)
